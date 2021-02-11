d6b6fe1e1c9788eec5bd45955593431155e6d0ef
Melissa Lawrence <mlawrence537@gmail.com>
2021-02-11 21:46:58 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,8 +62,8 @@ def runner
   card_total = hit?(card_total)
   until card_total > 21 
   hit?(card_total) 
+  display_card_total(card_total)
 end
-display_card_total(card_total)
 end_game(card_total)
 end
     
